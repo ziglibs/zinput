4cee18891cddfac5da3378ca0e7dfc51415195f1
SuperAuguste <auguste.rame@gmail.com>
2020-05-24 20:36:38 -0400

    add ask dir path

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
 
+/// Caller must free memory.
 pub fn askString(allocator: *std.mem.Allocator, prompt: []const u8, max_size: usize) ![]u8 {
     const in = std.io.getStdIn().inStream();
     const out = std.io.getStdOut().outStream();
@@ -10,6 +11,29 @@ pub fn askString(allocator: *std.mem.Allocator, prompt: []const u8, max_size: us
 
     const result = try in.readUntilDelimiterAlloc(allocator, '\n', max_size);
     return if (std.mem.endsWith(u8, result, "\r")) result[0..(result.len - 1)] else result;
+}
+
+/// Caller must free memory. Max size is recommended to be a high value, like 512.
+pub fn askDirPath(allocator: *std.mem.Allocator, prompt: []const u8, max_size: usize) ![]u8 {
+    const out = std.io.getStdOut().outStream();
+
+    while (true) {
+        const path = try askString(allocator, prompt, max_size);
+        if (!std.fs.path.isAbsolute(path)) {
+            _ = try out.write("Error: Invalid directory, please try again.\n\n");
+            allocator.free(path);
+            continue;
+        }
+        
+        var dir = std.fs.cwd().openDir(path, std.fs.Dir.OpenDirOptions{}) catch {
+            _ = try out.write("Error: Invalid directory, please try again.\n\n");
+            allocator.free(path);
+            continue;
+        };
+
+        dir.close();
+        return path;
+    }
 }
 
 pub fn askBool(prompt: []const u8) !bool {
@@ -40,13 +64,11 @@ test "basic input functionality" {
 
     std.debug.warn("Welcome to the ZLS configuration wizard! (insert mage emoji here)\n", .{});
 
-    const stdp = try askString(testing.allocator, "What is your Zig lib path (path that contains the 'std' folder)?", 128);
+    const stdp = try askDirPath(testing.allocator, "What is your Zig lib path (path that contains the 'std' folder)?", 128);
     const snippet = try askBool("Do you want to enable snippets?");
     const style = try askBool("Do you want to enable style warnings?");
 
     defer testing.allocator.free(stdp);
-
-    std.debug.warn("{} {} {}", .{stdp, snippet, style});
 
     // std.debug.warn("{} {} {}", .{stdp, snippet, style});
 
