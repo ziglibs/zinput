c7641fadd09d589b0d05e196aa46e133032d6a7f
TechPriest <yaminogakusei@gmail.com>
2021-02-19 22:57:25 +0200

    Basic ColorWriter impl

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,116 @@ const std = @import("std");
 const ansi = @import("ansi.zig");
 const testing = std.testing;
 
+const windows = std.os.windows;
+// https://docs.microsoft.com/en-us/windows/console/getconsolemode
+extern "kernel32" fn GetConsoleMode(h_console: windows.HANDLE, mode: *windows.DWORD) callconv(if (std.builtin.arch == .i386) .Stdcall else .C) windows.BOOL;
+
+pub fn terminalSupportsAnsiColor(h_console: std.os.fd_t) bool {
+    if (std.builtin.os.tag == .windows) {
+        var mode: windows.DWORD = 0;
+        if (GetConsoleMode(h_console, &mode) != windows.FALSE) {
+            const ENABLE_VIRTUAL_TERMINAL_PROCESSING: windows.DWORD = 0x0004;
+            if (mode & ENABLE_VIRTUAL_TERMINAL_PROCESSING != 0)
+                return true;            
+        } 
+
+        // Check if we run under ConEmu
+        const wstr = std.unicode.utf8ToUtf16LeStringLiteral;
+        if (std.os.getenvW(wstr("ConEmuANSI"))) |val| {
+            if (std.mem.eql(u16, val, wstr("ON")))
+                return true;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+const Fg = enum {
+    Reset,
+
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    LightGray,
+    Default,
+    DarkGray,
+    LightRed,
+    LightGreen,
+    LightYellow,
+    LightBlue,
+    LightMagenta,
+    LightCyan,
+    White,
+
+    fn ansiValue(self: Fg) []const u8 {
+        return switch (self) {
+            Fg.Reset => ansi.Reset(),
+            Fg.Black => ansi.Foreground(ansi.Black),
+            Fg.Red => ansi.Foreground(ansi.Red),
+            Fg.Green => ansi.Foreground(ansi.Green),
+            Fg.Yellow => ansi.Foreground(ansi.Yellow),
+            Fg.Blue => ansi.Foreground(ansi.Blue),
+            Fg.Magenta => ansi.Foreground(ansi.Magenta),
+            Fg.Cyan => ansi.Foreground(ansi.Cyan),
+            Fg.LightGray => ansi.Foreground(ansi.LightGray),
+            Fg.Default => ansi.Foreground(ansi.Default),
+            Fg.DarkGray => ansi.Foreground(ansi.DarkGray),
+            Fg.LightRed => ansi.Foreground(ansi.LightRed),
+            Fg.LightGreen => ansi.Foreground(ansi.LightGreen),
+            Fg.LightYellow => ansi.Foreground(ansi.LightYellow),
+            Fg.LightBlue => ansi.Foreground(ansi.LightBlue),
+            Fg.LightMagenta => ansi.Foreground(ansi.LightMagenta),
+            Fg.LightCyan => ansi.Foreground(ansi.LightCyan),
+            Fg.White => ansi.Foreground(ansi.White),
+        };
+    }
+};
+
+const ColorWriter = struct {
+    const Self = @This();
+
+    writer: std.fs.File.Writer,
+    have_color: bool,
+
+    pub fn init(output: std.fs.File) Self {
+        return Self {
+            .writer = output.writer(),
+            .have_color = terminalSupportsAnsiColor(output.handle),
+        };
+    }
+
+    pub fn writeSeq(self: *const Self, seq: anytype) !void {
+        comptime var i: usize = 0;
+        inline while (i < seq.len) : (i += 1) {
+            const val = seq[i];
+            switch (@TypeOf(val)) {
+                Fg => {
+                    if (self.have_color) {
+                        try self.writeAll(val.ansiValue());
+                    }
+                },
+                else => try self.writeAll(val),
+            }
+        }
+    }
+
+    pub fn writeAll(self: *const Self, val: []const u8) !void {
+        try self.writer.writeAll(val);
+    }
+};
+
 /// Caller must free memory.
 pub fn askString(allocator: *std.mem.Allocator, prompt: []const u8, max_size: usize) ![]u8 {
     const in = std.io.getStdIn().reader();
-    const out = std.io.getStdOut().writer();
+    const out = ColorWriter.init(std.io.getStdOut());
 
-    try out.writeAll(ansi.Foreground(ansi.Cyan) ++ "? " ++ ansi.Foreground(ansi.White));
-    try out.writeAll(prompt);
-    try out.writeAll(ansi.Foreground(ansi.DarkGray) ++ " > " ++ ansi.Reset());
+    try out.writeSeq(.{ Fg.Cyan, "? ", Fg.White, prompt, Fg.Reset });
 
     const result = try in.readUntilDelimiterAlloc(allocator, '\n', max_size);
     return if (std.mem.endsWith(u8, result, "\r")) result[0..(result.len - 1)] else result;
@@ -17,18 +119,18 @@ pub fn askString(allocator: *std.mem.Allocator, prompt: []const u8, max_size: us
 
 /// Caller must free memory. Max size is recommended to be a high value, like 512.
 pub fn askDirPath(allocator: *std.mem.Allocator, prompt: []const u8, max_size: usize) ![]u8 {
-    const out = std.io.getStdOut().writer();
+    const out = ColorWriter.init(std.io.getStdOut());
 
     while (true) {
         const path = try askString(allocator, prompt, max_size);
         if (!std.fs.path.isAbsolute(path)) {
-            try out.writeAll(ansi.Foreground(ansi.Red) ++ "Error: Invalid directory, please try again.\n\n" ++ ansi.Reset());
+            try out.writeSeq(.{ Fg.Red, "Error: Invalid directory, please try again.\n\n", Fg.Reset });
             allocator.free(path);
             continue;
         }
 
         var dir = std.fs.cwd().openDir(path, std.fs.Dir.OpenDirOptions{}) catch {
-            try out.writeAll(ansi.Foreground(ansi.Red) ++ "Error: Invalid directory, please try again.\n\n" ++ ansi.Reset());
+            try cw.writeSequence(.{ Fg.Red, "Error: Invalid directory, please try again.\n\n", Fg.Reset});
             allocator.free(path);
             continue;
         };
@@ -40,14 +142,12 @@ pub fn askDirPath(allocator: *std.mem.Allocator, prompt: []const u8, max_size: u
 
 pub fn askBool(prompt: []const u8) !bool {
     const in = std.io.getStdIn().reader();
-    const out = std.io.getStdOut().writer();
+    const out = ColorWriter.init(std.io.getStdOut());
 
     var buffer: [1]u8 = undefined;
 
     while (true) {
-        try out.writeAll(ansi.Foreground(ansi.Cyan) ++ "? " ++ ansi.Foreground(ansi.White));
-        try out.writeAll(prompt);
-        try out.writeAll(ansi.Foreground(ansi.DarkGray) ++ " (y/n) > " ++ ansi.Reset());
+        try out.writeSeq(.{ Fg.Cyan, "? ", Fg.White, prompt, Fg.DarkGray, " (y/n) > ", Fg.Reset });
 
         const read = in.read(&buffer) catch continue;
         try in.skipUntilDelimiterOrEof('\n');
@@ -64,29 +164,24 @@ pub fn askBool(prompt: []const u8) !bool {
 
 pub fn askSelectOne(prompt: []const u8, comptime options: type) !options {
     const in = std.io.getStdIn().reader();
-    const out = std.io.getStdOut().writer();
+    const out = ColorWriter.init(std.io.getStdOut());
 
-    try out.writeAll(ansi.Foreground(ansi.Cyan) ++ "? " ++ ansi.Foreground(ansi.White));
-    try out.writeAll(prompt);
-    try out.writeAll(ansi.Foreground(ansi.DarkGray) ++ " (select one)" ++ ansi.Reset() ++ "\n\n");
+    try out.writeSeq(.{ Fg.Cyan, "? ", Fg.White, prompt, Fg.DarkGray, " (select one)", Fg.Reset, "\n\n" });
 
     comptime var max_size: usize = 0;
     inline for (@typeInfo(options).Enum.fields) |option| {
-        try out.writeAll("  - ");
-        try out.writeAll(option.name);
-        try out.writeAll("\n");
-
+        try out.writeSeq(.{ "  - ", option.name, "\n" });
         if (option.name.len > max_size) max_size = option.name.len;
     }
 
     while (true) {
         var buffer: [max_size + 1]u8 = undefined;
 
-        try out.writeAll(ansi.Foreground(ansi.DarkGray) ++ "\n>" ++ ansi.Reset() ++ " ");
+        try out.writeSeq(.{ Fg.DarkGray, "\n>", Fg.Reset, " " });
 
         var result = (in.readUntilDelimiterOrEof(&buffer, '\n') catch {
             try in.skipUntilDelimiterOrEof('\n');
-            try out.writeAll(ansi.Foreground(ansi.Red) ++ "Error: Invalid option, please try again.\n" ++ ansi.Reset());
+            try out.writeSeq(.{ Fg.Red, "Error: Invalid option, please try again.\n", Fg.Reset });
             continue;
         }) orelse return error.EndOfStream;
         result = if (std.mem.endsWith(u8, result, "\r")) result[0..(result.len - 1)] else result;
@@ -96,7 +191,7 @@ pub fn askSelectOne(prompt: []const u8, comptime options: type) !options {
                 return @intToEnum(options, option.value);
         // return option.value;
 
-        try out.writeAll(ansi.Foreground(ansi.Red) ++ "Error: Invalid option, please try again.\n" ++ ansi.Reset());
+        try out.writeSeq(.{ Fg.Red, "Error: Invalid option, please try again.\n", Fg.Reset });
     }
 
     // return undefined;
